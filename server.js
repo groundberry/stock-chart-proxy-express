bf3a1537c2bf22d03d6a330543ba0f84d0c9ff6c
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2019-01-10 16:32:54 -0800

    Create server

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const request = require("request");
 
-app.get('/', (req, res) => res.send('Welcome to StockChart!'))
+require("dotenv").config();
 
-app.listen(port, () => console.log(`StockChart app listening on port ${port}!`))
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  const companyName = req.query["companyName"];
+  const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${companyName}&types=quote,news,chart&range=1m&last=5`;
+
+  request(url, (err, resIEX, body) => {
+    if (!err && resIEX.statusCode === 200) {
+      res.send(body);
+    } else {
+      next(err);
+    }
+  });
+});
+
+app.listen(port, () =>
+  console.log(`StockChart app listening on port ${port}!`)
+);
