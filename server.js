4eec56ce7b63ca038e9ee88c8b02410b358f115d
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2020-11-25 10:46:10 -0800

    Migrate to new IEX Cloud API

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const token = process.env.TOKEN || '???';
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
@@ -14,7 +15,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   const symbol = req.query["symbol"];
   const range = req.query["range"];
-  const url = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${symbol}&types=quote,chart&range=${range}`;
+  const url = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/${range}?token=${token}`;
 
   request(url).pipe(res);
 });
